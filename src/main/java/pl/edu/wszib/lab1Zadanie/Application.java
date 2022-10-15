036ce3b5b8dcc2ace2342ca44a208b6328b6ba9b
waleryn <polaker2@gmail.com>
2022-10-15 14:55:16 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/lab1Zadanie/Application.java
@@ -0,0 +1,11 @@
+package pl.edu.wszib.lab1Zadanie;
+
+public class Application {
+
+    public static void main(String[] args) {
+        koktajlTruskawkowy koktajl = new koktajlTruskawkowy();
+        koktajl.jedz();
+        koktajl.pij();
+        koktajl.smak();
+    }
+}
